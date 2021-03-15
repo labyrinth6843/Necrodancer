45e381eeb4dc8082db9a8e28e19f101243c10030
labyrinth6843 <79181772+labyrinth6843@users.noreply.github.com>
2021-03-15 17:40:21 +0900

    플레이어 임시

--- 01_WinMain/Player.cpp
+++ 01_WinMain/Player.cpp
@@ -8,58 +8,96 @@ void Player::Init() {
 	mHeadImage = ImageManager::GetInstance()->FindImage(L"Head");
 	mBodyImage = ImageManager::GetInstance()->FindImage(L"Body");
 
+	mHeadLeftAnimation = new Animation();
+	mHeadLeftAnimation->InitFrameByStartEnd(4, mArmor,7, mArmor,false);
+	mHeadLeftAnimation->SetIsLoop(true);
+	mHeadLeftAnimation->Play();
+
+	mHeadRightAnimation = new Animation();
+	mHeadRightAnimation->InitFrameByStartEnd(0, mArmor,3, mArmor,false);
+	mHeadRightAnimation->SetIsLoop(true);
+	mHeadRightAnimation->Play();
+
+	mBodyLeftAnimation = new Animation();
+	mBodyLeftAnimation->InitFrameByStartEnd(4, mArmorType, 7, mArmorType, false);
+	mBodyLeftAnimation->SetIsLoop(true);
+	mBodyLeftAnimation->Play();
+
+	mBodyRightAnimation = new Animation();
+	mBodyRightAnimation->InitFrameByStartEnd(0, mArmorType,3, mArmorType,false);
+	mBodyRightAnimation->SetIsLoop(true);
+	mBodyRightAnimation->Play();
+
 	mHp = 6;
 	mX = 100 + TileSize * 5;
 	mY = 200 + TileSize * 4;
-	indexX = mX / TileSize;
-	indexY = mY / TileSize;
+	initindexX = mX / TileSize;
+	initindexY = mY / TileSize;
+
+	mCurrentHeadAnimation = mHeadRightAnimation;
+	mCurrentBodyAnimation = mBodyRightAnimation;
 }
 
 void Player::Release() { 
-
+	SafeDelete(mHeadLeftAnimation)
+	SafeDelete(mHeadRightAnimation)
+	SafeDelete(mBodyLeftAnimation)
+	SafeDelete(mBodyRightAnimation)
 }
 
 void Player::Update() {
-	indexX = (mX - 100) / TileSize;
-	indexY = (mY - 200) / TileSize;
+	initindexX = (mX - 100) / TileSize;
+	initindexY = (mY - 200) / TileSize;
 
 	if (mIsMove == false) {
-		if (Input::GetInstance()->GetKeyDown(VK_UP) || Input::GetInstance()->GetKeyDown('W')) {
-			if (indexY > 0)
+		if (Input::GetInstance()->GetKeyDown(VK_UP) || Input::GetInstance()->GetKeyDown('W'))
+			if (initindexY > 0)
 				Move(0, -1);
-		}
+
+		if (Input::GetInstance()->GetKeyDown(VK_RIGHT) || Input::GetInstance()->GetKeyDown('D'))
+			if (initindexX < 9) {
+				if (direction == false) {
+					direction = true;
+					mCurrentHeadAnimation = mHeadRightAnimation;
+					mCurrentBodyAnimation = mBodyRightAnimation;
+				}
+				Move(1, 0);
+			}
 	
 		if (Input::GetInstance()->GetKeyDown(VK_LEFT) || Input::GetInstance()->GetKeyDown('A')) {
-			if(indexX > 0)
+			if (initindexX > 0) {
+				if (direction == true) {
+					direction = false;
+					mCurrentHeadAnimation = mHeadLeftAnimation;
+					mCurrentBodyAnimation = mBodyLeftAnimation;
+				}
 				Move(-1, 0);
+			}
 		}
 			
 		if (Input::GetInstance()->GetKeyDown(VK_DOWN) || Input::GetInstance()->GetKeyDown('S'))
-			if(indexY < 9)
+			if (initindexY < 9)
 				Move(0, 1);
-	
-		if (Input::GetInstance()->GetKeyDown(VK_RIGHT) || Input::GetInstance()->GetKeyDown('D'))
-			if(indexX < 9)
-				Move(1, 0);
 	}
 	else {
-		mX += TileSize*2 * Time::GetInstance()->DeltaTime() *  cosf(Math::GetAngle(mX, mY, destX, destY));
-		mY += TileSize*2 * Time::GetInstance()->DeltaTime() * -sinf(Math::GetAngle(mX, mY, destX, destY));
+		mX += TileSize*2 * Time::GetInstance()->DeltaTime() *  cosf(Math::GetAngle(mX, mY, destindexX, destindexY));
+		mY += TileSize*2 * Time::GetInstance()->DeltaTime() * -sinf(Math::GetAngle(mX, mY, destindexX, destindexY));
 
-		if (fabs(destX - mX) <= 0.5 && fabs(destY - mY) <= 0.5) {
-			mX = destX;
-			mY = destY;
+		if (fabs(destindexX - mX) <= 0.5 && fabs(destindexY - mY) <= 0.5) {
+			mX = destindexX;
+			mY = destindexY;
 			mIsMove = false;
 		}
 	}
 }
 
 void Player::Render(HDC hdc) {
-
+	mBodyImage->TileFrameRender(hdc, initindexX, initindexY, mCurrentBodyAnimation->GetNowFrameX(), mCurrentBodyAnimation->GetNowFrameY());
+	mHeadImage->TileFrameRender(hdc, initindexX, initindexY, mCurrentHeadAnimation->GetNowFrameX(), mCurrentHeadAnimation->GetNowFrameY());
 }
 
 void Player::Move(int x, int y) {
-	destX = mX + TileSize * x;
-	destY = mY + TileSize * y;
+	destindexX = mX + TileSize * x;
+	destindexY = mY + TileSize * y;
 	mIsMove = true;
 }
--- 01_WinMain/Player.h
+++ 01_WinMain/Player.h
@@ -1,16 +1,34 @@
 #pragma once
 #include "Image.h"
 #include "GameObject.h"
+#include "Animation.h"
 
 class Player:public GameObject{
 	Image* mHeadImage;
 	Image* mBodyImage;
 
-	int mHp;
-	int indexX;
-	int indexY;
-	float destX;
-	float destY;
+	bool mArmor = false;
+	int mArmorType = 0;
+
+	bool direction = true;
+
+	Animation* mHeadLeftAnimation;
+	Animation* mHeadRightAnimation;
+	Animation* mBodyLeftAnimation;
+	Animation* mBodyRightAnimation;
+
+	Animation* mCurrentHeadAnimation;
+	Animation* mCurrentBodyAnimation;
+	
+	float mHp = 6;
+	float mDmg = 1;
+	float mDef = 0;
+
+	int mCoin = 0;
+	int initindexX;
+	int initindexY;
+	float destindexX;
+	float destindexY;
 	bool mIsMove;
 
 	int mSpeed;
@@ -23,4 +41,16 @@ public:
 	void Render(HDC hdc)override;
 
 	void Move(int x, int y);
+
+	void SetHp(float hp) { mHp = hp; }
+	float GetHp() { return mHp; }
+
+	float GetDmg() { return mDmg; }
+
+	float GetDef() { return mDef; }
+
+	POINT GetCord() { return { initindexX, initindexY }; }
+
+	void SetCoin(int coin) { mCoin = coin; }
+	int GetCoin() { return mCoin; }
 };
