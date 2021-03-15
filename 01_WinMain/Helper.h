b0fdbc6ad7d70070a37aa9ddd31328ea3d29e7de
chasoyoung <chasoyoung221@gmail.com>
2021-03-15 17:07:34 +0900

    Helper.h 함수 소수 추가
    Queue초기화, wstring<->string 함수

--- 01_WinMain/Helper.h
+++ 01_WinMain/Helper.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <stack>
-
+#include <queue>
 template<typename T >inline void StackClear(stack< T >& Stacks)
 {
 	while (false == Stacks.empty())
@@ -8,3 +8,25 @@ template<typename T >inline void StackClear(stack< T >& Stacks)
 		Stacks.pop();
 	}
 };
+
+template<typename T >inline void QueueClear(stack< T >& Queue)
+{
+	while (false == Queue.empty())
+	{
+		Queue.pop();
+	}
+};
+
+inline string to_string(wstring wst)
+{
+	string st;
+	st.assign(wst.begin(), wst.end());
+	return st;
+}
+
+inline wstring to_string(string st)
+{
+	wstring wst;
+	wst.assign(st.begin(), st.end());
+	return wst;
+}
