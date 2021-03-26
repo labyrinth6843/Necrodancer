658d10bc7ded8609b23ad67f6cc1422f112c8465
labyrinth6843 <79181772+labyrinth6843@users.noreply.github.com>
2021-03-26 17:38:28 +0900

    빠진 항목 추가

--- 01_WinMain/Minotaur.cpp
+++ 01_WinMain/Minotaur.cpp
@@ -1,2 +1,42 @@
 #include "pch.h"
 #include "Minotaur.h"
+
+Minotaur::Minotaur(const string & name, int x, int y):Enemy(name)
+{
+	mX = x * TileSize;
+	mY = y * TileSize;
+
+	mHp = 3;
+	mCoin = 10;
+	mAtk = 2.f;
+
+	mImage = ImageManager::GetInstance()->FindImage(L"Slime2");
+}
+
+void Minotaur::Init()
+{
+
+}
+
+void Minotaur::Update()
+{
+
+}
+
+void Minotaur::Release()
+{
+
+}
+
+void Minotaur::Render(HDC hdc)
+{
+
+}
+
+void Minotaur::Attack()
+{
+}
+
+void Minotaur::IsAttacked(int dmg)
+{
+}
--- 01_WinMain/Minotaur.h
+++ 01_WinMain/Minotaur.h
@@ -1,5 +1,17 @@
 #pragma once
-class Minotaur
+#include "Enemy.h"
+class Minotaur:public Enemy
 {
-};
+public:
 
+public:
+	Minotaur(const string& name, int x, int y);
+
+	void Attack()override;
+	void IsAttacked(int dmg)override;
+
+	void Init()override;
+	void Update()override;
+	void Release()override;
+	void Render(HDC hdc)override;
+};
